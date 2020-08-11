1f5cf7af875d37cae34ec49e0ed7f91c703e3c6c
Masfik <contact@masfik.net>
2020-08-11 12:01:13 +0100

    Re-formatted indentation (2 spaces)

--- tg-backup.sh
+++ tg-backup.sh
@@ -14,34 +14,34 @@ disable_notification=true
 # Telegram endpoint
 telegram="https://api.telegram.org/bot$BOT_TOKEN"
 # Current directory of the script
-current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+current_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 # Creating the ./backup-scripts directory if non-existent
 if [ ! -d "$current_dir"/backup-scripts ]; then
-  mkdir -p "$current_dir"/backup-scripts;
+  mkdir -p "$current_dir"/backup-scripts
 fi
 
 # Creating the ./backup-files directory if non-existent
 if [ ! -d "$current_dir"/backup-files ]; then
-  mkdir -p "$current_dir"/backup-files;
+  mkdir -p "$current_dir"/backup-files
 fi
 
 # Creating the ./thumbnails directory if non-existent
 if [ ! -d "$current_dir"/thumbnails ]; then
-  mkdir -p "$current_dir"/thumbnails;
+  mkdir -p "$current_dir"/thumbnails
 fi
 
 #-------------------------------------------------------------------------------
 # RUNNING ALL BACKUP SCRIPTS INSIDE THE ./backup-scripts DIRECTORY
 # 1) All scripts must end with the .sh extension
-# 2) All backed up files must be .zip archives and placed inside the 
-#    ./backup-files directory 
-# 3) ... 
+# 2) All backed up files must be .zip archives and placed inside the
+#    ./backup-files directory
+# 3) ...
 #-------------------------------------------------------------------------------
 
 for backupScript in "$current_dir"/backup-scripts/*.sh; do
-    # Passing the backup directory to all scripts
-    bash "$backupScript" "$current_dir/backup-files" -H || break
+  # Passing the backup directory to all scripts
+  bash "$backupScript" "$current_dir/backup-files" -H || break
 done
 
 #-------------------------------------------------------------------------------
@@ -49,23 +49,23 @@ done
 #-------------------------------------------------------------------------------
 
 for file in "$current_dir"/backup-files/*.zip; do
-    # Only continue the loop if there are files inside the directory
-    test -f "$file" || continue
+  # Only continue the loop if there are files inside the directory
+  test -f "$file" || continue
 
-    # Removing the .zip extension from the file name
-    file_name=${file%.zip}
-    # Removing the full path from the file name
-    file_name=${file_name##*/}
+  # Removing the .zip extension from the file name
+  file_name=${file%.zip}
+  # Removing the full path from the file name
+  file_name=${file_name##*/}
 
-    # Thumbnail of the file
-    thumbnail="$current_dir/thumbnails/$file_name.jpg"
+  # Thumbnail of the file
+  thumbnail="$current_dir/thumbnails/$file_name.jpg"
 
-    # Sending the file to the Telegram chat
-    curl "$telegram/sendDocument" \
-        -F chat_id=$CHAT_ID \
-        -F document=@"$file" \
-        -F caption="#$file_name: $(date +%d/%m/%Y)" \
-        -F thumb=@"$thumbnail" \
-        -F disable_notification=$disable_notification \
-        -H "Content-Type: multipart/form-data"
+  # Sending the file to the Telegram chat
+  curl "$telegram/sendDocument" \
+    -F chat_id="$CHAT_ID" \
+    -F document=@"$file" \
+    -F caption="#$file_name: $(date +%d/%m/%Y)" \
+    -F thumb=@"$thumbnail" \
+    -F disable_notification=$disable_notification \
+    -H "Content-Type: multipart/form-data"
 done
