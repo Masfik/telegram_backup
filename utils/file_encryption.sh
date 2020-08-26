3268c4c44fa0fe72cbe270bc6489f5f837db8e46
Masfik <contact@masfik.net>
2020-08-26 14:13:45 +0200

    Created separate encrypt_file() function

--- utils/file_encryption.sh
+++ utils/file_encryption.sh
@@ -1,7 +1,37 @@
 #!/bin/bash
+# Create an encrypted file with GnuPG.
+# The first parameter is the file name.
+# The second parameter must be --gpg-recipients, followed by a list of valid
+# E-Mail addresses saved.
+#
+# Usage example:
+# encrypt_file file.txt --gpg-recipients email1@domain.tld email2@domain.tld
+function encrypt_file() {
+  if [[ "$2" != "--gpg-recipients" ]]; then
+    echo "The second argument is not --gpg-recipients."
+    echo "Example: encrypt_file file.txt --gpg-recipients email@domain.tld"
+    exit 1
+  fi
+
+  local file_path=$1
+  local file_name="${file_path%%.*}" # ← Removing extension
+  shift 2
+
+  echo "$file_name"
+
+  recipients_param=""
+  for recipient in "$@"; do
+    recipients_param+="--recipient $recipient "
+  done
+
+  # Encrypting file
+  # shellcheck disable=SC2086
+  gpg --output "$file_name.gpg" --encrypt --sign $recipients_param "$file_path"
+}
+
 # Create an encrypted zip archive with GnuPG.
-# The first argument must be the name of the zip file, followed by the other
-# arguments related entirely to the `zip` command.
+# The first argument must be the name of the zip file (must end with .zip ext),
+# followed by the other arguments related entirely to the `zip` command.
 # The last few arguments must be related to gpg recipients.
 # !!! THE ORDER IS IMPORTANT !!!
 #
@@ -12,10 +42,10 @@
 #   \
 #   --gpg-recipients email1@domain.tld email2@domain.tld email3@domain.tld
 function encrypted_zip() {
-  file_name=$1
+  local file_name=$1
 
   # If the first parameter does NOT end with the .zip extension
-  if [[ "$file_name.zip" != *.zip ]]; then
+  if [[ "$file_name" != *.zip ]]; then
     echo "The first argument does not indicate a .zip file name."
     echo "Example: encrypted_zip test.zip ..."
     exit 1
@@ -28,9 +58,6 @@ function encrypted_zip() {
       --gpg-recipients email@domain.tld"
     exit 1
   fi
-
-  # Removing the .zip extension from the file name
-  file_name=${file_name%.zip}
 
   # Splitting function parameters into an array.
   # The first element will be dedicated to the zip command parameters.
@@ -45,16 +72,9 @@ function encrypted_zip() {
   # Creating a "recipients" array using the space as the delimiter
   IFS=" " read -r -a recipients <<<"${params[1]}"
 
-  # Creating the --recipient parameter for each recipient
-  recipients_param=""
-  for recipient in "${recipients[@]}"; do
-    recipients_param+="--recipient $recipient "
-  done
-
-  # Encrypting zip archive
-  # shellcheck disable=SC2086
-  gpg --output "$file_name.gpg" --encrypt --sign $recipients_param "$file_name.zip"
+  # Encrypting ZIP archive
+  encrypt_file "$file_name" --gpg-recipients "${recipients[@]}"
 
   # Removing zip and only keeping the encrypted file
-  rm "$file_name.zip"
+  rm "$file_name"
 }
