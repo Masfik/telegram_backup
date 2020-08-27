b3a296db65fed2ad834d690fc8d3ffe0f1668b22
Masfik <contact@masfik.net>
2020-08-27 13:42:47 +0200

    generate_config() now supports array * -i replaced with -s for strings, whereas the new -a is for arrays

--- utils/generate_config.sh
+++ utils/generate_config.sh
@@ -9,13 +9,17 @@ function generate_config() {
   # Defining local variables (the "g" prefix is required to avoid conflicts)
   local g_arg g_config_file g_config_items=()
 
-  # Function named arguments (-f for "config File" and -i for "config Item")
-  while getopts ":f:i:" g_arg; do
+  # Function named arguments
+  # -f for "config File"
+  # -s for "String" item
+  # -a for "Array" item
+  while getopts ":f:s:a:" g_arg; do
     case ${g_arg} in
     f) g_config_file=${OPTARG} ;;
-    i) g_config_items+=("${OPTARG}") ;;
+    s) g_config_items+=(["${OPTARG}"]="string") ;;
+    a) g_config_items+=(["${OPTARG}"]="array") ;;
     \?)
-      echo "Usage: generate_config [-f -i]"
+      echo "Usage: generate_config [-f -s -a]"
       return 1
       ;;
     esac
@@ -26,15 +30,28 @@ function generate_config() {
     mkdir -p "$(dirname "$g_config_file")"
   fi
 
-  # Creating the config file if non-existent and appending comments
+  # Creating the config file if non-existent
   if [[ ! -f "$g_config_file" ]]; then
     touch "$g_config_file"
     {
       echo "#!/bin/bash"
-      echo "# IMPORTANT: only change the value between the \"quotation marks\"."
+      echo -n "# IMPORTANT: only change the value between the"
+      echo "\"quotation marks\" or \"()\"."
     } >"$g_config_file"
 
-    # Adding each config item specified by the "-i" argument
+    local len=${#g_config_items[@]} # ← Length of the array
+
+    # Adding each config item specified with the -s or -a argument
+    for ((i = 0; i < $len; i++)); do
+      if [[ "${g_config_items[$i]}" == "string" ]]; then
+        # Appending item to file and wrapping value around quotation marks
+        echo "export ${!g_config_items[$i]}" | sed 's/=/=\"/; s/$/\"/' \
+          >>"$g_config_file"
+      elif [[ "${g_config_items[$i]}" == "array" ]]; then
+        echo "export ${!g_config_items[$i]}" >>"$g_config_file"
+      fi
+    done
+
     for item in "${g_config_items[@]}"; do
       # Appending config item to file and wrapping value around quotation marks
       echo "export $item" | sed 's/=/=\"/; s/$/\"/' >>"$g_config_file"
